89a329b1fb502c1828e48cb500e664e203c7cea8
Adrian Ivasku <Adrian.Ivasku@rt-rk.com>
2019-06-12 11:58:11 +0200

    Fragment setUp

--- app/src/main/java/com/tms/androidfragments2/MainActivity.java
+++ app/src/main/java/com/tms/androidfragments2/MainActivity.java
@@ -1,13 +1,19 @@
 package com.tms.androidfragments2;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Fragment_1.OnFragmentInteractionListener, Fragment_2.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
